862f88dc4cb5368659735ddd81badb0c09332570
Himonn <67947663+Himonn@users.noreply.github.com>
2024-02-19 19:15:10 +0000

    add deleteall method

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { put } = require('@vercel/blob');
+const { put, list, del } = require('@vercel/blob');
 const async = require('async');
 require('dotenv').config();
 
@@ -32,4 +32,25 @@ async function uploadFiles(directory, startIndex = 0) {
     files.slice(startIndex).forEach(file => uploadQueue.push(file));
 }
 
-uploadFiles('./items-icons', 1000);
+// uploadFiles('./items-icons', 1000);
+
+
+async function deleteAll() {
+    try {
+        const files = await list({token: process.env.BLOB_READ_WRITE_TOKEN});
+        const urls = Object.values(files.blobs).map(blob => blob.url);
+
+        await async.eachLimit(urls, 10, async url => {
+            try {
+                await del(url, {token: process.env.BLOB_READ_WRITE_TOKEN});
+                console.log(`Deleted ${url}`);
+            } catch (e) {
+                console.error(`Failed to delete ${url}: ${e}`);
+            }
+        });
+    } catch (err) {
+        console.error(`Failed to list files: ${err}`);
+    }
+}
+
+deleteAll();
